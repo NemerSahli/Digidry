28eab46ed78b1907364fb23328b3aafa154058b4
Nemer EL-Sahli <nemer.sahli@gmx.de>
2018-10-14 13:14:11 +0200

    user label added

--- public/main.js
+++ public/main.js
@@ -30,6 +30,7 @@ $("#login-btn-id").click(() => {
 
       success: function(res) {
         console.log("response", res);
+        $("#label-user-id").html("User: " + user.username);
 
         if (res.error == 0) {
           hideForms();
